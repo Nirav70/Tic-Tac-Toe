d981b6ea1beb9f79e411c58cc8952f2c87fb6d69
Nirav70 <82320892+Nirav70@users.noreply.github.com>
2021-10-14 22:27:22 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Starting game");
-        System.out.println("Manav Khatri...");
+        System.out.println("Manav ko nahi milegi Khatri...");
 
     }
 }
